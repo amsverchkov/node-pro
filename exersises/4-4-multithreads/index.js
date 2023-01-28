d8336c9ae88c10a5bd1d7ed7b17ca55785c43ef9
Sverchkov <risunochki@yandex.ru>
2023-01-28 09:22:59 +0300

    Правки после code review

--- exersises/4-4-multithreads/index.js
+++ exersises/4-4-multithreads/index.js
@@ -3,6 +3,8 @@ const splitArr = require('./split-arr');
 const { performance, PerformanceObserver } = require('perf_hooks');
 const { Worker } = require('worker_threads');
 
+const coresNumber = 16;
+
 const perfObserver = new PerformanceObserver((list) => {
     list.getEntries().forEach((entry) => {
         console.log(`${entry.name} duration ${entry.duration}`);
@@ -11,20 +13,21 @@ const perfObserver = new PerformanceObserver((list) => {
 
 perfObserver.observe({ entryTypes: ['measure'] });
 
-
-function sqr(array) {
-    return array.map((x) => (Math.random() > 0.5 ? x * 2 : x / 3));
-}
-
 const intitArr = [...Array(30000000).keys()];
 
-performance.mark('startCalc');
-const result = complexCalculate({ arr: intitArr });
-performance.mark('stopCalc');
-performance.measure('calcSum', 'startCalc', 'stopCalc');
+//Here are complex calcing with and withou workers
+calcWithoutWorkers({ arr: intitArr })
+calcWithFewWorkers(intitArr);
 
-const calcWithWorker = (intitArr) => {
-    return new Promise((resolve, reject) => {
+function calcWithoutWorkers(initObj) {
+    performance.mark('startCalc');
+    complexCalculate(initObj);
+    performance.mark('stopCalc');
+    performance.measure('calcSum', 'startCalc', 'stopCalc');
+}
+
+function calcWithWorker (intitArr) {
+    return new Promise((resolve) => {
         const myWorker = new Worker('./worker.js', {
             workerData: { arr: intitArr }
         })
@@ -32,31 +35,11 @@ const calcWithWorker = (intitArr) => {
     })
 }
 
-
-const calcWithFewWorkers = async (intitArr) => {
-    splitedIntitArr = splitArr(intitArr, 16);
+async function calcWithFewWorkers (intitArr) {
+    splitedIntitArr = splitArr(intitArr, coresNumber);
     performance.mark('startCalcWorkers');
-    const results = await Promise.all([
-        calcWithWorker(splitedIntitArr[0]),
-        calcWithWorker(splitedIntitArr[1]),
-        calcWithWorker(splitedIntitArr[2]),
-        calcWithWorker(splitedIntitArr[3]),
-        calcWithWorker(splitedIntitArr[4]),
-        calcWithWorker(splitedIntitArr[5]),
-        calcWithWorker(splitedIntitArr[6]),
-        calcWithWorker(splitedIntitArr[7]),
-        calcWithWorker(splitedIntitArr[8]),
-        calcWithWorker(splitedIntitArr[9]),
-        calcWithWorker(splitedIntitArr[10]),
-        calcWithWorker(splitedIntitArr[11]),
-        calcWithWorker(splitedIntitArr[12]),
-        calcWithWorker(splitedIntitArr[13]),
-        calcWithWorker(splitedIntitArr[14]),
-        calcWithWorker(splitedIntitArr[15]),
-    ]);
-
+    await Promise.all(splitedIntitArr.map(arr => calcWithWorker(arr)));
     performance.mark('stopCalcWorkers');
     performance.measure('calcSumWorkers', 'startCalcWorkers', 'stopCalcWorkers');
 }
 
-calcWithFewWorkers(intitArr);
