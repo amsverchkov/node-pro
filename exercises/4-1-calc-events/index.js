09fd7fb486b10d67103580e30e32cfe7093c5791
Sverchkov <risunochki@yandex.ru>
2023-02-13 11:29:33 +0300

    Перенес в подписку функцию валидации

--- exercises/4-1-calc-events/index.js
+++ exercises/4-1-calc-events/index.js
@@ -1,11 +1,11 @@
 const EventEmiter = require('events');
-const {getValidationErrors, castParamsToFloat} = require('../4-1-calc/index');
-const {add} = require('../4-1-calc/add');
-const {divide} = require('../4-1-calc/divide');
-const {multiply} = require('../4-1-calc/multiply');
-const {subtract} = require('../4-1-calc/subtract');
+const { getValidationErrors, castParamsToFloat } = require('../4-1-calc/index');
+const { add } = require('../4-1-calc/add');
+const { divide } = require('../4-1-calc/divide');
+const { multiply } = require('../4-1-calc/multiply');
+const { subtract } = require('../4-1-calc/subtract');
 
-let [,,firstNum, secondNum, operation] = process.argv;
+let [, , firstNum, secondNum, operation] = process.argv;
 
 const myEmiter = new EventEmiter();
 
@@ -13,6 +13,17 @@ myEmiter.on('error', errorHandler);
 
 myEmiter.on('result', (result) => {
     console.log(`Operation result is: ${result}`);
+})
+
+myEmiter.on('validate', () => {
+    const errors = getValidationErrors(firstNum, secondNum, operation);
+    if (errors.length > 0) {
+        myEmiter.emit(
+            'error',
+            new Error(errors.join(' '))
+        );
+        return;
+    }
 })
 
 myEmiter.on('add', (firstNum, secondNum) => {
@@ -33,14 +44,7 @@ myEmiter.on('subtract', (firstNum, secondNum) => {
     myEmiter.emit('result', subtract.getResult(firstNum, secondNum));
 });
 
-const errors = getValidationErrors(firstNum, secondNum, operation);
-if (errors.length > 0) {
-    myEmiter.emit(
-        'error', 
-        new Error(errors.join(' '))    
-    );
-    return;
-}
+myEmiter.emit('validate', firstNum, secondNum, operation);
 
 [firstNum, secondNum] = castParamsToFloat(firstNum, secondNum);
 
